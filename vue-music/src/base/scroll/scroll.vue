1eb277e17b81090abb081502f22d7e26f0f43d99
95erlong <11700008@qq.com>
2017-09-15 17:58:01 +0800

    singerPage

--- vue-music/src/base/scroll/scroll.vue
+++ vue-music/src/base/scroll/scroll.vue
@@ -20,10 +20,14 @@
       data: {
         type: Array,
         default: null
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
       }
     },
     mounted() {
-      setTimeout(function () {
+      setTimeout(() => {
         this._initScroll()
       }, 20)
     },
@@ -36,6 +40,13 @@
           probeType: this.probeType,
           click: this.click
         })
+
+        if (this.listenScroll) {
+          let me = this
+          this.scroll.on('scroll', (pos) => {
+            me.$emit('scroll', pos)
+          })
+        }
       },
       enable() {
         this.scroll && this.scroll.enable()
@@ -45,11 +56,17 @@
       },
       refresh() {
         this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
       }
     },
     watch: {
       data() {
-        setTimeout(function () {
+        setTimeout(() => {
           this.refresh()
         }, 20)
       }
